d9ab78aee15723efad1517639066cda601d28700
jdspell <jdspell@iu.edu>
2023-11-25 18:43:33 -0500

    added variable types and declarations

--- beejguide/hello.c
+++ beejguide/hello.c
@@ -4,5 +4,9 @@
 
 int main(void)
 {
+	int i = 2; // signed integers
+	float f = 3.14; // signed floating point numbers
+	char *s = "Hello World!"; // char* (char pointer) is the string type
 	printf("Hello world\n"); // imma comment
+	printf("%s i = %d and f = %f!\n", s, i, f);
 }
