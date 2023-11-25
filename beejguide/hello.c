7f8c188a1458a8e3ffdb2cfa34fc1ebd655a4787
jdspell <jdspell@iu.edu>
2023-11-25 18:57:56 -0500

    added sizeof checks

--- beejguide/hello.c
+++ beejguide/hello.c
@@ -1,9 +1,18 @@
 /*Hello world program*/
 
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(void)
 {
+	bool x = true;
+	if (x) {
+		printf("x is true! \n");
+	}
+	int a = 9999;
+	printf("%zu\n", sizeof a);
+	printf("%zu\n", sizeof(2 + 7));
+	printf("%zu\n", sizeof 3.14);
 	int i = 2; // signed integers
 	float f = 3.14; // signed floating point numbers
 	char *s = "Hello World!"; // char* (char pointer) is the string type
